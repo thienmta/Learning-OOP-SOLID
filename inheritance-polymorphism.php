993e4c0ca6feb1f07573c4c9c4e7cb2d1a437cbf
ThienMTA <thienmta1406@gmail.com>
2018-03-30 00:36:54 +0700

    commit class extends MaleStudent

---
+++ inheritance-polymorphism.php
@@ -0,0 +1,57 @@
+<?php
+abstract class Student{
+	public $name;
+	public $age;
+	protected $score;
+
+	function Student($name, $age){
+		$this->name = $name;
+		$this->age = $age;
+	}
+
+	function setScore($score){
+		if($score>=0 && $score<=10){
+			$this->score = $score;
+		}
+		else{
+			$this->score = 0;
+		}
+	}
+
+	function getStudent(){
+		echo "Name student: ".$this->name;
+		echo "Age student: ".$this->age;
+
+		$checkScore = $this->score;
+
+		if($checkScore >=0 && $checkScore<4){
+			echo "Hoc luc: Yeu";
+		}
+		elseif($checkScore >=4 && $checkScore<7){
+			echo "Hoc luc: Trung binh";
+		}
+		elseif($checkScore >=7 && $checkScore<9){
+			echo "Hoc luc: Kha";
+		}
+		elseif($checkScore >=9 && $checkScore<=10){
+			echo "Hoc luc: Gioi";
+		}
+	}
+
+	abstract function getSex();
+}
+
+class MaleStudent extends Student{
+	public $gifted;
+	function MaleStudent($gifted){
+		$this->gifted = $gitted;
+	}
+
+	function getGifted(){
+		echo "Nang khieu: ".$this->gifted;
+	}
+
+	function getSex(){
+		echo "Gioi tinh: Nam";
+	}
+}
