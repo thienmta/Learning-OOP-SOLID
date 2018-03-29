acf610d46462e355f32d0ce3df484b563f9b6065
ThienMTA <thienmta1406@gmail.com>
2018-03-29 23:24:30 +0700

    commit function set-get Age

--- abstraction.php
+++ abstraction.php
@@ -7,5 +7,31 @@ class Person{
 	function Person($name){
 		$this->name = $name;
 	}
-	
+
+	function setAge($age){
+		if($age > 0){
+			$this->age = $age;
+		}
+		else{
+			$this->age = 0;
+		}
+	}
+
+	function getAge(){
+		if(0<$this->age<=10){
+			echo "Tre con";
+		}
+		if(10<$this->age<=15){
+			echo "Thieu nien";
+		}
+		if(15<$this->age<=20){
+			echo "Thanh nien";
+		}
+		if(20<$this->age<=50){
+			echo "Trung nien";
+		}
+		if(50<$this->age){
+			echo "Nguoi gia";
+		}
+	}
 }
