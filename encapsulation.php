a90419838493a5fd8722a43120e81c4e4f90da3e
ThienMTA <thienmta1406@gmail.com>
2018-03-30 00:53:55 +0700

    commit function khoi tao gia tri cho class Student

--- encapsulation.php
+++ encapsulation.php
@@ -1,5 +1,18 @@
 <?php
 class Student
 {
+	public $name;
+	protected $age;
+	private $score;
+
+	function Student($name, $age){
+		$this->name = $name;
+		$this->age = $age;
+	}
+
+	function setScore($score){
+		$this->score = $score;
+	}
+
 	
 }
