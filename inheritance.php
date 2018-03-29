8146d5a0a54947380e8c7c49bd5fa64b1f42b6bb
ThienMTA <thienmta1406@gmail.com>
2018-03-30 00:17:13 +0700

    commit function khoi tao

--- inheritance.php
+++ inheritance.php
@@ -1,4 +1,16 @@
 <?php
 class Student{
-	
+	public $name;
+	public $age;
+	private $score;
+
+	function Student($name, $age){
+		$this->name = $name;
+		$this->age = $age;
+	}
+
+	function setScore($score){
+		$this->score = $score;
+	}
+
 }
