954c385b68bed9a83722633bedcfc7da1dba54e5
ThienMTA <thienmta1406@gmail.com>
2018-03-30 01:20:38 +0700

    create class setNameAge anh getNameAge

--- single_responsibility_principle.php
+++ single_responsibility_principle.php
@@ -6,3 +6,18 @@ class SetStudent
 	protected $score;
 }
 
+class setNameAge extends SetStudent{
+	function setNameAge($name, $age){
+		$this->name = $name;
+		$this->age = $age;
+	}
+}
+
+class getNameAge extends setNameAge{
+	function getNameAge(){
+		echo "Name".$this->name;
+		echo "Age".$this->age;
+	}
+}
+
+
