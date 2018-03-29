2b52fc5f625de1b3fe655630641a65e4eb7c1b45
ThienMTA <thienmta1406@gmail.com>
2018-03-30 00:10:17 +0700

    edit function getAge and data test

--- abstraction.php
+++ abstraction.php
@@ -21,16 +21,31 @@ class Person{
 	}
 
 	function getAge(){
-		echo "Age: ". $this->age;
+		$checkAge = $this->age;
+		if($checkAge >= 0 && $checkAge <10){
+			echo "Tre con ";
+		}
+		elseif($checkAge >= 10 && $checkAge <15){
+			echo "Thieu nien";
+		}
+		elseif($checkAge >= 15 && $checkAge <20){
+			echo "Thanh nien";
+		}
+		elseif($checkAge >= 20 && $checkAge <50){
+			echo "Trung nien";
+		}
+		elseif($checkAge >= 50){
+			echo "Lao nien";
+		}
 	}
 }
 
 $thien = new Person("Nguyen Van Thien");
 $thien->getPerson();
-$thien->setAge(20);
+$thien->setAge(19);
 $thien->getAge();
 
 $ha = new Person("Nguyen Thi Ha");
 $ha->getPerson();
-$ha->setAge(20);
+$ha->setAge(45);
 $ha->getAge();
--- inheritance.php
+++ inheritance.php
@@ -1,4 +1,4 @@
 <?php
-abstract class Student{
+class Student{
 	
 }
