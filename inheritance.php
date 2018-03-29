7303cca3edf83bded2835e2175d23a11eec8a4fd
ThienMTA <thienmta1406@gmail.com>
2018-03-30 00:25:27 +0700

    edit function setScore and function getStudent

--- inheritance.php
+++ inheritance.php
@@ -2,7 +2,7 @@
 class Student{
 	public $name;
 	public $age;
-	private $score;
+	protected $score;
 
 	function Student($name, $age){
 		$this->name = $name;
@@ -10,7 +10,31 @@ class Student{
 	}
 
 	function setScore($score){
-		$this->score = $score;
+		if($score>=0 && $score<=10){
+			$this->score = $score;
+		}
+		else{
+			$this->score = 0;
+		}
 	}
 
+	function getStudent(){
+		echo "Name student: ".$this->name;
+		echo "Age student: ".$this->age;
+
+		$checkScore = $this->score;
+
+		if($checkScore >=0 && $checkScore<4){
+			echo "Hoc luc: Yeu";
+		}
+		elseif($checkScore >=4 && $checkScore<7){
+			echo "Hoc luc: Trung binh";
+		}
+		elseif($checkScore >=7 && $checkScore<9){
+			echo "Hoc luc: Kha";
+		}
+		elseif($checkScore >=9 && $checkScore<=10){
+			echo "Hoc luc: Gioi";
+		}
+	}
 }
