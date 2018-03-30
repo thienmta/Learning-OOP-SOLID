697e0ceb55611ac7e2d83d642f9ace5284be6a93
ThienMTA <thienmta1406@gmail.com>
2018-03-30 09:10:21 +0700

    commit class Student and Child extends Person

--- open_closed_principle.php
+++ open_closed_principle.php
@@ -1,4 +1,34 @@
 <?php
-class Student{
-	
+class Person{
+	public $name;
+
+	function Person($name){
+		$this->name = $name;
+	}
+
+	function getNamePerson(){
+		echo "Name: ".$this->name;
+	}
+}
+
+class Student extends Person{
+	private $age;
+	function Student($age){
+		$this->age = $age;
+	}
+
+	function getStudent(){
+		echo "Age: ".$this->age;
+	}
+}
+
+class Child extends Person{
+	private $sex;
+	function Student($sex){
+		$this->sex = $sex;
+	}
+
+	function getStudent(){
+		echo "Sex: ".$this->sex;
+	}
 }
