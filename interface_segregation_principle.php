ecda307a5c55315a088ce54f6275b22181eb3728
ThienMTA <thienmta1406@gmail.com>
2018-03-30 10:16:45 +0700

    commit interface ban dau

---
+++ interface_segregation_principle.php
@@ -0,0 +1,18 @@
+<?php
+interface Name{
+	function setName($name);
+	function getName();
+
+}
+
+interface Sex{
+	function setName($sex);
+	function getName();
+
+}
+
+interface Color{
+	function setName($color);
+	function getName();
+
+}
