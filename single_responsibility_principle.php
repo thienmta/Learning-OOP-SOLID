0ea289a6cf5eaa513a69f6b431a83432f536e09d
ThienMTA <thienmta1406@gmail.com>
2018-03-30 11:55:56 +0700

    edit file single_responsibility_principle.php

--- single_responsibility_principle.php
+++ single_responsibility_principle.php
@@ -1,40 +1,44 @@
 <?php
-class SetStudent
+class Student
 {
-	public $name;
-	public $age;
+	protected $name;
+	protected $age;
 	protected $score;
-}
 
-class setNameAge extends SetStudent{
-	function setNameAge($name, $age){
+	function Student($name, $age, $score) {
 		$this->name = $name;
 		$this->age = $age;
+		if(0 <= $score && $score <= 10) {
+			$this->score = $score;
+		}
+		else {
+			$this->score = 0;
+		}
 	}
 }
 
-class getNameAge extends setNameAge{
-	function getNameAge(){
-		echo "Name: ".$this->name;
-		echo "Age: ".$this->age;
+class SayStudent extends Student {
+	function showStudent(){
+		echo "Name: ".$this->name."\n";
+		echo "Age: ".$this->age."\n";
+
+		$checkScore = $this->score;
+
+		if($checkScore >=0 && $checkScore<4){
+			echo "Hoc luc: Yeu \n";
+		}
+		elseif($checkScore >=4 && $checkScore<7){
+			echo "Hoc luc: Trung binh\n";
+		}
+		elseif($checkScore >=7 && $checkScore<9){
+			echo "Hoc luc: Kha\n";
+		}
+		elseif($checkScore >=9 && $checkScore<=10){
+			echo "Hoc luc: Gioi\n";
+		}
 	}
+
 }
 
-class setScore extends getNameAge{
-	function setScore($score){
-		$this->score = $score;
-	}
-}
-
-class getScore extends setScore{
-	function getScoreStudent(){
-		echo "Score: ".$this->score;
-	}
-}
-
-$score = new getScore();
-$score->setScore(7);
-$score->setNameAge("Nguyen Van Thien", 22);
-$score->getNameAge();
-$score->getScoreStudent();
-
+$myStudent = new SayStudent("Nguyen Van Thien", 21, 7);
+$myStudent->showStudent();
